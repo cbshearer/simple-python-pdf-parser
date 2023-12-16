749aeed2d0e278c893d8bc4140eb3499dc69778a
Chris Shearer <cbshearer@gmail.com>
2023-12-15 17:28:13 -0800

    Create pdf_combine.py

---
+++ pdf_combine.py
@@ -0,0 +1,22 @@
+from PyPDF2 import PdfReader, PdfWriter
+import os
+
+source_dir = "c:\\users\\chris\\pdfs"
+target_dir = "c:\\temp"
+target_file = "out.pdf"
+merged_pdf = PdfWriter()
+
+for filename in os.listdir(source_dir):
+    if filename.endswith(".pdf"):
+        print(f"Combining: {filename}")
+        absolute_path = os.path.join(source_dir, filename)
+        with open(absolute_path, 'rb') as f:
+            reader = PdfReader(f)
+            for page in reader.pages:
+                merged_pdf.add_page(page)
+
+with open(f"{target_dir}\\{target_file}", 'wb') as merged_file:
+    merged_pdf.write(merged_file)
+print("#######################################")
+print(f"Successfully merged PDFs from\n\"{source_dir}\" \ninto \n\"{target_dir}\\{target_file}\"")
+print("#######################################")
